d9144ce2dad13a646508340d5a8eb14248d60c77
CHINRAJ13 <chinrajmit@gmail.com>
2025-06-14 13:18:22 +0530

    Initial Commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import ContactSection from "../components/ContactSection";
+import HeaderImage from "../components/HeadImage";
+import MapSection from "../components/MapSection";
+
+export default function Contact() {
+    return (
+        <>
+        <HeaderImage page={'Contact'} />
+        <MapSection />
+        <ContactSection />
+        </>
+    )
+}
