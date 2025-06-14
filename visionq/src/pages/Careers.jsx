60f9a756b39c167617960a89a1f35d99890995e3
CHINRAJ13 <chinrajmit@gmail.com>
2025-06-14 18:40:12 +0530

    Deploy changes commit

---
+++ visionq/src/pages/Careers.jsx
@@ -0,0 +1,12 @@
+import CareerSection from "../components/CareerSection";
+import HeaderImage from "../components/HeadImage";
+
+
+export default function Careers() {
+    return (
+        <>
+        <HeaderImage page={'Career'} />
+        <CareerSection />
+        </>
+    )
+}
---
+++ visionq/src/pages/Services.jsx
@@ -0,0 +1,13 @@
+import HeaderImage from "../components/HeadImage";
+import KeyFeatures from "../components/KeyFeatures";
+import ServicesSection from "../components/ServicesSection";
+
+export default function Services() {
+    return (
+        <>
+        <HeaderImage page={'Service'} />
+        <ServicesSection />
+        <KeyFeatures />
+        </>
+    )
+}
